514fadd40f3e5fffab2f65297c9842fa8ef4568b
Joshua Burt <joshburt@users.noreply.github.com>
2022-09-15 12:49:26 -0700

    allowed type and details in the same object optionally (#12)

--- src/rowantree/contracts/dto/user/feature_state.py
+++ src/rowantree/contracts/dto/user/feature_state.py
@@ -3,6 +3,7 @@
 from typing import Optional
 
 from ...feature_details import FeatureDetails
+from ...feature_type import FeatureType
 from ..base_model import BaseModel
 
 
@@ -14,5 +15,6 @@ class UserFeatureState(BaseModel):
     ----------
     """
 
-    details: FeatureDetails
+    name: Optional[FeatureType]
+    details: Optional[FeatureDetails]
     description: Optional[str]
